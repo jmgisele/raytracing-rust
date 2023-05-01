a00b2c9dcb5c86a85e4d7a4ff53a901af4234a52
James Gisele <james.may.gisele@protonmail.com>
2023-05-01 18:38:17 -0400

    chapter 3 refactor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use nalgebra::Vector3;
+
 fn main() {
     const IMAGE_WIDTH: u32 = 256;
     const IMAGE_HEIGHT: u32 = 256;
@@ -8,17 +10,25 @@ fn main() {
     for j in (0..IMAGE_WIDTH).rev() {
         eprintln!("Scanlines remaining: {j}");
         for i in 0..IMAGE_HEIGHT {
-            let r: f32 = i as f32 / IMAGE_WIDTH as f32;
-            let g: f32 = j as f32 / IMAGE_HEIGHT as f32;
-            let b: f32 = 0.25;
+            let color = Color(Vector3::new(
+                i as f64 / IMAGE_WIDTH as f64,
+                j as f64 / IMAGE_HEIGHT as f64,
+                0.25,
+            ));
 
-            let i_r: i32 = (255.999 * r) as i32;
-            let i_g: i32 = (255.999 * g) as i32;
-            let i_b: i32 = (255.999 * b) as i32;
-
-            println!("{i_r} {i_g} {i_b} \n");
+            write_color(color);
         }
     }
-
     eprintln!("Done!");
+}
+
+struct Color(Vector3<f64>);
+struct Point(Vector3<f64>);
+
+fn write_color(color: Color) {
+    let r = 255.999 * color.0.x;
+    let g = 255.999 * color.0.y;
+    let b = 255.999 * color.0.z;
+
+    println!("{r} {g} {b} \n");
 }
